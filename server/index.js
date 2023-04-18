3992932329f4b1e1bf751b8433b198b078ecfee9
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-18 08:32:17 -0600

    Connection to  Mysql ready (locally)

--- server/index.js
+++ server/index.js
@@ -2,16 +2,45 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const fs = require("fs");
+const mysql = require('mysql');
 
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+let connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'actividad_7'
+});
+
+
+
 app.use(bodyParser.json());
+
+/* connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].solution);
+}); */
+ 
+
 
 app.get("/api/movies", (req, res) => {
   fs.readFile(__dirname + "/" + "json/movies.json", "utf8", (err, data) => {
     res.end(data);
   })
+})
+
+// ACTIVIDAD 7
+app.get("/api/users", (req, res) => {
+  connection.connect();
+  connection.query('SELECT * FROM users', function (error, results, fields) {
+    if (error) throw error;
+    console.log('The solution is: ', results);
+    res.end(JSON.stringify(results));
+  });
+  connection.end();
 })
 
 app.post("/api/movies", (req, res) => {
@@ -22,3 +51,5 @@ app.post("/api/movies", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+//connection.end();
