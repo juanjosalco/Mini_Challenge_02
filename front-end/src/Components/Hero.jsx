e92dad6185deafa2481bfb6c47e73ae27db7dcaa
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-19 11:07:44 -0600

    Hero page finished

---
+++ front-end/src/Components/Hero.jsx
@@ -0,0 +1,15 @@
+export const Hero = () => {
+    return (
+        <div className="hero min-h-screen bg-base-200">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="text-5xl font-bold">Hello there</h1>
+                    <p className="py-6">
+                        View our movie catalog
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
