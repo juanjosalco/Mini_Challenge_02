3ac9d48a7d84a5026c0c6fb7df09e79ab7faf0b5
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-19 16:59:57 -0600

    Server connected to planet scale

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
 import { Hero } from './Components/Hero';
 import { Navbar } from './Components/Navbar';
+import Movie from './Components/Movie';
 
 function App() {
   return(
     <div>
-      <Navbar />
-      <Hero />
-      <Hero />
+      <Movie />
     </div>
   )
 }
--- server/index.js
+++ server/index.js
@@ -2,49 +2,60 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
-const mysql = require("mysql");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-let connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "actividad_7",
-});
+require('dotenv').config()
+const mysql = require('mysql2')
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+console.log('Connected to PlanetScale!')
 
 app.use(bodyParser.json());
 
-/* connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-}); */
-
 app.get("/api/movies", (req, res) => {
-    fs.readFile(__dirname + "/" + "json/movies.json", "utf8", (err, data) => {
-        res.end(data);
+    connection.connect(function(err) {
+        if (err) throw err;
+        console.log("Connected!");
+        connection.query("SELECT * FROM movies", function (err, result, fields) {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+        });
     });
-});
+})
 
-// ACTIVIDAD 7
-app.get("/api/users", (req, res) => {
-    connection.connect();
-    connection.query("SELECT * FROM users", function (error, results, fields) {
-        if (error) throw error;
-        console.log("The solution is: ", results);
-        res.end(JSON.stringify(results));
-    });
-    connection.end();
-});
 
-app.post("/api/movies", (req, res) => {
-    console.log("El cuerpo de la peticion:", req.body);
-    res.send(req.body);
-});
+
+// /* connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// }); */
+
+// app.get("/api/movies", (req, res) => {
+//     fs.readFile(__dirname + "/" + "json/movies.json", "utf8", (err, data) => {
+//         res.end(data);
+//     });
+// });
+
+// // ACTIVIDAD 7
+// app.get("/api/users", (req, res) => {
+//     connection.connect();
+//     connection.query("SELECT * FROM users", function (error, results, fields) {
+//         if (error) throw error;
+//         console.log("The solution is: ", results);
+//         res.end(JSON.stringify(results));
+//     });
+//     connection.end();
+// });
+
+// app.post("/api/movies", (req, res) => {
+//     console.log("El cuerpo de la peticion:", req.body);
+//     res.send(req.body);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
 
-//connection.end();
+// //connection.end();
