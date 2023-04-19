5730f0753c43df35c0f47aec3acb1c9c5b659e4b
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-19 13:35:24 -0600

    Added href to a tags

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -12,12 +12,12 @@ export const Navbar = () => {
     return (
         <nav className="navbar bg-base-100 fixed z-50">
             <div className="flex-1">
-                <a className="btn btn-ghost normal-case text-xl">Blockbuster</a>
+                <a href="/" className="btn btn-ghost normal-case text-xl">Blockbuster</a>
             </div>
             <div className="flex-none">
                 <ul className="menu menu-horizontal px-1">
                     <li>
-                        <a>Agregar Película</a>
+                        <a href="/">Agregar Película</a>
                     </li>
                     <li tabIndex={0}>
                         <div>
