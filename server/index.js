a382a09d2173c4869537cdef49e382832e2fa5ca
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-19 17:56:22 -0600

    Prepared to redirect to the build folder through the backend

--- server/index.js
+++ server/index.js
@@ -5,27 +5,39 @@ const fs = require("fs");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const path = require("path");
 
-require('dotenv').config()
-const mysql = require('mysql2')
-const connection = mysql.createConnection(process.env.DATABASE_URL)
-console.log('Connected to PlanetScale!')
+require("dotenv").config();
+const mysql = require("mysql2");
+const connection = mysql.createConnection(process.env.DATABASE_URL);
+console.log("Connected to PlanetScale!");
 
 app.use(bodyParser.json());
 
 app.get("/api/movies", (req, res) => {
-    connection.connect(function(err) {
+    connection.connect(function (err) {
         if (err) throw err;
         console.log("Connected!");
-        connection.query("SELECT * FROM movies", function (err, result, fields) {
-            if (err) throw err;
-            console.log(result);
-            res.send(result);
-        });
+        connection.query(
+            "SELECT * FROM movies",
+            function (err, result, fields) {
+                if (err) throw err;
+                console.log(result);
+                res.send(result);
+            }
+        );
     });
-})
+});
 
 
+// Esto hace que NodeJS sirva los archivos resultado del build de ReactJS
+// Esto va antes de nuestros endpoints pero después de la declaración de app.
+app.use(express.static(path.resolve(__dirname, "../front-end/build")));
+// Todas las peticiones GET que no manejamos ahora regresarán nuestra React App
+// Agrega esto antes del “app.listen”
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../front-end/build", "index.html"));
+});
 
 // /* connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 //   if (error) throw error;
