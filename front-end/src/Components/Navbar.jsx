caa1ac42f400e033890b3cc7886e711b58d6aa0c
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-19 14:01:19 -0600

    Navbar updated with necessary values

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,14 +1,4 @@
 export const Navbar = () => {
-    const movieTypes = [
-        {
-            tipo: "Acción",
-            href: "/movies/accion",
-        },
-        {
-            tipo: "Comedia",
-            href: "/movies/comedia",
-        },
-    ];
     return (
         <nav className="navbar bg-base-100 fixed z-50">
             <div className="flex-1">
@@ -19,30 +9,8 @@ export const Navbar = () => {
                     <li>
                         <a href="/">Agregar Película</a>
                     </li>
-                    <li tabIndex={0}>
-                        <div>
-                            Ver Películas
-                            <svg
-                                className="fill-current"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                            </svg>
-                        </div>
-                        <ul className="p-2 bg-base-100">
-                            {movieTypes.map((movieType, index) => {
-                                return (
-                                    <li key={index}>
-                                        <a href={movieType.href}>
-                                            {movieType.tipo}
-                                        </a>
-                                    </li>
-                                );
-                            })}
-                        </ul>
+                    <li>
+                        <a href="/">Ver Películas</a>
                     </li>
                 </ul>
             </div>
