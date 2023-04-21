3e7b2b346461dc0e1bdcf5ab2ea44e4fe35a8300
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-21 10:14:43 -0600

    fetches data from database

--- front-end/src/Components/MovieCard.jsx
+++ front-end/src/Components/MovieCard.jsx
@@ -4,17 +4,17 @@ export const MovieCard = ({ id, title, description, image, rating }) => {
     const movie = { id, title, description, image, rating };
     return (
         <>
-            <div className="card bg-base-300 shadow-xl p-2 mx-10 sm:mx-0 flex flex-grow flex-1 max-h-96">
-                <figure>
+            <div className="card bg-base-300 shadow-xl p-2 mx-10 sm:mx-0  flex flex-grow flex-1 max-h-96">
+                <figure className="min-h-[150px] max-h-[150px]">
                     <img
                         src={image}
                         alt={title}
                         className="rounded-md object-cover w-full h-64"
                     />
                 </figure>
-                <div className="card-body">
-                    <h2 className="card-title min-h-[50px]">{title}</h2>
-                    <p>{description}</p>
+                <div className="card-body min-h-[100px]">
+                    <h2 className="card-title ">{title}</h2>
+                    <p className="overflow-clip">{description}</p>
                     <div className="card-actions justify-center mt-2">
                         <label className="btn btn-secondary" htmlFor={`my-modal-${id}`}>Edit</label>
                     </div>
