72ca40110b1fd028e2c879c8546daf0fed78d472
Juan Salazar <100889614+juanjosalco@users.noreply.github.com>
2023-04-21 07:36:33 -0600

    Revert "deploy"
    This reverts commit 04ba50ebd52916e7625be7f19589b3d1d113cec1.

--- index.js
+++ index.js
@@ -29,14 +29,42 @@ app.get("/api/movies", (req, res) => {
     });
 });
 
+
 // Esto hace que NodeJS sirva los archivos resultado del build de ReactJS
 // Esto va antes de nuestros endpoints pero después de la declaración de app.
-app.use(express.static(path.resolve("client/build")));
+app.use(express.static(path.resolve("../front-end/build")));
 // Todas las peticiones GET que no manejamos ahora regresarán nuestra React App
 // Agrega esto antes del “app.listen”
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve("client/build", "index.html"));
+    res.sendFile(path.resolve("../front-end/build", "index.html"));
 });
+
+// /* connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
+//   if (error) throw error;
+//   console.log('The solution is: ', results[0].solution);
+// }); */
+
+// app.get("/api/movies", (req, res) => {
+//     fs.readFile(__dirname + "/" + "json/movies.json", "utf8", (err, data) => {
+//         res.end(data);
+//     });
+// });
+
+// // ACTIVIDAD 7
+// app.get("/api/users", (req, res) => {
+//     connection.connect();
+//     connection.query("SELECT * FROM users", function (error, results, fields) {
+//         if (error) throw error;
+//         console.log("The solution is: ", results);
+//         res.end(JSON.stringify(results));
+//     });
+//     connection.end();
+// });
+
+// app.post("/api/movies", (req, res) => {
+//     console.log("El cuerpo de la peticion:", req.body);
+//     res.send(req.body);
+// });
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
