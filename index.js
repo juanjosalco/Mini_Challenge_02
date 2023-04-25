0f3cc44f24a31072f7182d7af5f46ededd005feb
Armando Terrazas Gómez <A01640924@tec.mx>
2023-04-25 10:33:58 -0600

    Endpoints code ready. Missing some testing

--- index.js
+++ index.js
@@ -29,6 +29,54 @@ app.get("/api/movies", (req, res) => {
     });
 });
 
+app.post("/api/movies", (req, res) => {
+    connection.connect(function (err) {
+        if (err) throw err;
+        let query = `INSERT INTO movies (title, description, image, rating) VALUES (${req.body.title}, ${req.body.description}, ${req.body.image}, ${req.body.rating})`;
+        connection.query(
+            query, function (err, result, fields) {
+                if (err) res.status(500).send(err);
+                console.log(result);
+                res.send(result);
+            }
+        )
+    })
+})
+
+app.delete("/api/movies/:id", (req, res) => {
+    connection.connect(function (err) {
+        if (err) throw err;
+        let query = `DELETE FROM movies WHERE id = ${req.params.id}`;
+        connection.query(
+            query, function (err, result, fields) {
+                if (err) res.status(500).send(err);
+                console.log(result);
+                res.send(result);
+            }
+        )
+    })
+})
+
+app.patch("/api/movies/:id", (req,res) => {
+    connection.connect(function(err) {
+        if (err) throw err;
+        let query = `UPDATE movies SET `;
+        if (req.body.title) query += `title = ${req.body.title}, `;
+        if (req.body.description) query += `description = ${req.body.description}, `;
+        if (req.body.image) query += `image = ${req.body.image}, `;
+        if (req.body.rating) query += `rating = ${req.body.rating}, `;
+        query = query.slice(0, -2);
+        query += ` WHERE id = ${req.params.id}`;
+        connection.query(
+            query, function (err, result, fields) {
+                if (err) res.status(500).send(err);
+                console.log(result);
+                res.send(result);
+            }
+        )
+    })
+})
+
 // Esto hace que NodeJS sirva los archivos resultado del build de ReactJS
 // Esto va antes de nuestros endpoints pero después de la declaración de app.
 app.use(express.static(path.resolve("front-end/build")));
