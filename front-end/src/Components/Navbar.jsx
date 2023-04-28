53be0c5d91788badfad4ba7e5e2fabf982f308cb
Armando Terrazas Gómez <A01640924@tec.mx>
2023-04-28 14:42:47 -0600

    Ya puedes agregar una película

--- front-end/src/Components/Navbar.jsx
+++ front-end/src/Components/Navbar.jsx
@@ -1,19 +1,26 @@
+import { ModalEdit } from "./ModalEdit";
+import { useState } from "react";
+
 export const Navbar = () => {
+    const [modalOpen, setModalOpen] = useState(false);
     return (
-        <nav className="navbar bg-base-100 fixed z-50">
-            <div className="flex-1">
-                <a href="/" className="btn btn-ghost normal-case text-xl">Blockbuster</a>
-            </div>
-            <div className="flex-none">
-                <ul className="menu menu-horizontal px-1">
-                    <li>
-                        <a href="/">Agregar Película</a>
-                    </li>
-                    <li>
-                        <a href="#peliculas">Ver Películas</a>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+        <>
+            <nav className="navbar bg-base-100 fixed z-50">
+                <div className="flex-1">
+                    <a href="/" className="btn btn-ghost normal-case text-xl">Blockbuster</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li>
+                            <label onClick={() => setModalOpen(true)} htmlFor={`my-modal-create`}>Agregar Película</label>
+                        </li>
+                        <li>
+                            <a href="#peliculas">Ver Películas</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            {modalOpen && <ModalEdit id={"create"} closeModal={() => setModalOpen(false)} />}
+        </>
     );
 };
